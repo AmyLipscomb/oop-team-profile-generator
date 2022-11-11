be5b2fe99e778debb8b1a6b0cba20f3bdbfadfe4
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-11-11 08:49:14 -0500

    Added library folder to hold the js files for employee, engineer, manager, and intern

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const inquirer = require('inquirer');
 //^ This usually means you have to install the package. The package here that we need to install is 'Inquirer'
   //https://www.npmjs.com/package/inquirer
+
+  const Employee = require('./lib/employee')
+  const Manager = require('./lib/manager');
+  const Engineer = require('./lib/engineer');
+  const Intern = require('./lib/intern');
