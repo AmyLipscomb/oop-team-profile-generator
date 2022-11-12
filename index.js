8051c027bbda2ee871a750c176afbd62f32424f7
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-11-12 15:13:05 -0500

    entered header and box to html

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const inquirer = require('inquirer');
   const Manager = require('./lib/manager');
   const Engineer = require('./lib/engineer');
   const Intern = require('./lib/intern');
+  const fs = require('fs');
+
+  // const employeeList = [];
+
+  
